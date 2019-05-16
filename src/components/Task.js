ca8f8d3e04856d3c0287536792191fe21f6f0f7a
anil <anil.kadam@amrutraj.com>
2019-05-16 14:36:12 +0530

    taskbox UI

--- src/components/Task.js
+++ src/components/Task.js
@@ -13,8 +13,8 @@ function Task({ task: { id, title, state }, onArchiveTask, onPinTask }) {
         />
         <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
       </label>
-      <div className="title">
-        <input type="text" value={title} readOnly={true} placeholder="Input title" />
+      <div className="title" style={{background: "papayawhip"}}>
+        <input type="text" value={title} readOnly={true} placeholder="Input title" style={{ textOverflow: 'ellipsis' }} />
       </div>
 
       <div className="actions" onClick={event => event.stopPropagation()}>
--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import { withKnobs, object } from '@storybook/addon-knobs/react';
 
 import Task from './Task';
+
+const longTitle = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not`;
+
 
 export const task = {
   id: '1',
@@ -16,7 +20,21 @@ export const actions = {
   onArchiveTask: action('onArchiveTask'),
 };
 
-storiesOf('Task', module)
+// storiesOf('Task', module)
+//   .add('default', () => <Task task={task} {...actions} />)
+//   .add('pinned', () => <Task task={{ ...task, state: 'TASK_PINNED' }} {...actions} />)
+//   .add('archived', () => <Task task={{ ...task, state: 'TASK_ARCHIVED' }} {...actions} />);
+
+// storiesOf('Task', module)
+//   .addDecorator(withKnobs)
+//   .add('default', () => {
+//     return <Task task={object('task', { ...task })} {...actions} />;
+//   })
+//   .add('pinned', () => <Task task={{ ...task, state: 'TASK_PINNED' }} {...actions} />)
+//   .add('archived', () => <Task task={{ ...task, state: 'TASK_ARCHIVED' }} {...actions} />);
+
+  storiesOf('Task', module)
   .add('default', () => <Task task={task} {...actions} />)
   .add('pinned', () => <Task task={{ ...task, state: 'TASK_PINNED' }} {...actions} />)
-  .add('archived', () => <Task task={{ ...task, state: 'TASK_ARCHIVED' }} {...actions} />);
+  .add('archived', () => <Task task={{ ...task, state: 'TASK_ARCHIVED' }} {...actions} />)
+  .add('long title', () => <Task task={{ ...task, title: longTitle }} {...actions} />);
